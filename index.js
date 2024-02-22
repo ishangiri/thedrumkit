d4e8948c1bb5f8d6f53ad6ec56d721c4c9c504d7
ishangiri <81349942+ishangiri@users.noreply.github.com>
2024-02-22 12:53:36 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var numOfbuttons = document.querySelectorAll(".drum").length;
+var drum = document.querySelectorAll(".drum");
+for(let i = 0; i < numOfbuttons; i++){
+    drum[i].addEventListener("click", function(){
+      
+        let clickedButton = this.innerHTML;
+        makesound(clickedButton);
+    }
+    )}
+    
+document.addEventListener("keydown", (event)=>{
+  makesound(event.key)
+   
+})
+
+
+
+function makesound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("kick-bass.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("snare.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("crash.mp3");
+                audio.play();
+                break;
+        case "d":
+             var audio = new Audio("tom-1.mp3");
+                    audio.play();
+                    break;
+        case "j":
+             var audio = new Audio("tom-2.mp3");
+                    audio.play();
+                    break;
+        case "k":
+            var audio = new Audio("tom-3.mp3");
+                    audio.play();
+                    break;
+        case "l":
+            var audio = new Audio("tom-4.mp3");
+                    audio.play();
+                    break;
+                    default:
+       }
+}
